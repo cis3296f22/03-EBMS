fc9a42a7e314b8e8d1ad58bc34459851cbe9e8ed
Jacob Snarr <jacobsnarr@gmail.com>
2022-10-28 11:50:31 -0400

    Begin UI design, Header/NavBar, work on Firebase

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,10 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import React, {createContext, useState, useEffect} from 'react'
+// import useFirebaseAuth from'../config/useFirebaseAuth'
+// import { AuthUserProvider } from '../context/AuthUserContext'
+
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
