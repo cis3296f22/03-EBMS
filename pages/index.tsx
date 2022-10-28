a9a6ceaaf7e7e9a88b031c6a39f95a8bc436de76
Jacob Snarr <jacobsnarr@gmail.com>
2022-10-28 11:59:47 -0400

    Fix typo

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import React from 'react'
 import Link from 'next/link'
-import Header from '../components/header'
+import Header from '../components/Header'
 
 export default function Home() {
   return (
