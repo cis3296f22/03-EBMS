131c6428e37ed26f5e35b1d89e36cc815fd088d0
Jacob Snarr <jacobsnarr@gmail.com>
2022-11-10 12:06:11 -0500

    Add updated billboard design

--- components/Billboard.jsx
+++ components/Billboard.jsx
@@ -1,19 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import styles from './Billboard.module.css'
 
-function Billboard({imgSrc, size, rate, location}) {
+function Billboard({name, imgSrc, size, rate, location}) {
+  const [selected, setSelected] = useState(false);
 
   return (
-    <div className={styles.billboardContainer}>
+    <div className={selected ? `${styles.billboardContainer} ${styles.selected}` : styles.billboardContainer} onClick={() => { setSelected(!selected) }}>
       <img className={styles.billboardImage} src={imgSrc}></img>
       <div className={styles.descriptionContainer}>
-        <b> {size} </b> <br/>
-        { rate } <br/>
-        <i> {location} </i>
-      </div>
-      <div className={styles.selectButton}>
-        Select
+        {name + " in "}  <b> {location} </b>
+        <br/>
+        { size } <br/>
+        <i> {rate} </i>
       </div>
     </div>
   )
