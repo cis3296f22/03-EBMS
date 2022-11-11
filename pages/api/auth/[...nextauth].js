4ebd8d53351e489455e33935cd399d83a0af4a46
timlopesTU <tug92082@temple.edu>
2022-11-11 12:00:58 -0500

    My third attempt of fixing no secret error

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,7 +2,7 @@ import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 
 export default NextAuth({
-  secret: process.env.SECRET,
+  secret: process.env.JWT_SECRET,
   providers: [
     // OAuth authentication providers
     GoogleProvider({
