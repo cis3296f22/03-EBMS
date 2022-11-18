d6a86de2ec33a7205eaac4448d277e9580d03c76
timlopesTU <tug92082@temple.edu>
2022-11-18 13:28:48 -0500

    Using Supabase for user Authentication

--- components/Login.jsx
+++ components/Login.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import styles from './NavigationButton.module.css'
-import {session} from '@supabase/supabase-js'
-import LoginButton from '../pages/supabaseClient';
+import {supabase} from '../pages/supabaseClient';
 
+async function handleLogin() {
+  const { data, error } = await supabase.auth.signInWithOAuth({
+    provider: 'google'
+  })
+}
 
 const Login = () => {
 
   return (
     <a className={styles.navButton}>
-      <div id="SignIn" onClick={LoginButton}>
+      <div id="SignIn" onClick={handleLogin}>
       Sign In
       </div>
     </a>
--- pages/supabaseClient.js
+++ pages/supabaseClient.js
@@ -1,16 +1,18 @@
 import { createClient } from '@supabase/supabase-js'
 
-let supabase = createClient(
-  'https://uomcmbqkndtmleifmays.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVvbWNtYnFrbmR0bWxlaWZtYXlzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njg3MDUxNTYsImV4cCI6MTk4NDI4MTE1Nn0.WrbkHFkLzdv-YVIAG0qGbiwlHCqAdqr50OgQ83Y8aXs'
-)
+const supabaseUrl = 'https://uomcmbqkndtmleifmays.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVvbWNtYnFrbmR0bWxlaWZtYXlzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njg3MDUxNTYsImV4cCI6MTk4NDI4MTE1Nn0.WrbkHFkLzdv-YVIAG0qGbiwlHCqAdqr50OgQ83Y8aXs'
 
-const LoginButton = () => {
-  let supabase = createClient(
-    'https://uomcmbqkndtmleifmays.supabase.co',
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVvbWNtYnFrbmR0bWxlaWZtYXlzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njg3MDUxNTYsImV4cCI6MTk4NDI4MTE1Nn0.WrbkHFkLzdv-YVIAG0qGbiwlHCqAdqr50OgQ83Y8aXs'
-  )
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+
+//export default supabase
+
+// const LoginButton = () => {
+//   let supabase = createClient(
+//     'https://uomcmbqkndtmleifmays.supabase.co',
+//     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVvbWNtYnFrbmR0bWxlaWZtYXlzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njg3MDUxNTYsImV4cCI6MTk4NDI4MTE1Nn0.WrbkHFkLzdv-YVIAG0qGbiwlHCqAdqr50OgQ83Y8aXs'
+//   )
   
-  supabase.auth.signInWithOAuth({ provider: 'google' })
-}
-export default LoginButton
+//   supabase.auth.signInWithOAuth({ provider: 'google' })
+// }
+// export default LoginButton
