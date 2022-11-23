263d1bd032e9147934554bbff2cd79f528322ba3
Jacob Snarr <jacobsnarr@gmail.com>
2022-11-23 01:27:18 -0500

    Overhaul design - Add CSS variables to standardize application colors - Organize homepage - Add Billboard Details component

--- components/Billboard.jsx
+++ components/Billboard.jsx
@@ -3,10 +3,11 @@ import React, { useState } from 'react'
 import styles from './Billboard.module.css'
 
 function Billboard({name, imgSrc, size, rate, location}) {
-  const [selected, setSelected] = useState(false);
+  // const [selected, setSelected] = useState(false);
+  // let selectedStyles = selected ? `${styles.billboardContainer} ${styles.selected}` : styles.billboardContainer;
 
   return (
-    <div className={selected ? `${styles.billboardContainer} ${styles.selected}` : styles.billboardContainer} onClick={() => { setSelected(!selected) }}>
+    <div className={styles.billboardListView}>
       <img className={styles.billboardImage} src={imgSrc}></img>
       <div className={styles.descriptionContainer}>
         {name + " in "}  <b> {location} </b>
---
+++ components/BillboardDetails.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import styles from './Billboard.module.css'
+
+const BillboardDetails = () => {
+  return (
+    <div className={styles.billboardFullView}>
+      Test
+    </div>
+  )
+}
+
+export default BillboardDetails;
