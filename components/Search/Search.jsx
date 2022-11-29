e976a52dc2791f74f3e7abed25e4557386ccc988
Jacob Snarr <jacobsnarr@gmail.com>
2022-11-29 03:03:04 -0500

    Refactor Search.jsx and remove unused imports

--- components/Search/Search.jsx
+++ components/Search/Search.jsx
@@ -1,9 +1,9 @@
 
-import { useState, useEffect } from 'react'
-import { useUser, useSupabaseClient } from '@supabase/auth-helpers-react'
+import { useState } from 'react'
+import { useSupabaseClient } from '@supabase/auth-helpers-react'
 import BillboardList from '../Billboard/BillboardList/BillboardList';
 
-export default function Search({session}) {
+export default function Search() {
     const supabase = useSupabaseClient()
     const user = useUser()
     const [loading, setLoading] = useState(false)
@@ -11,40 +11,33 @@ export default function Search({session}) {
     const [listArray, setListArray] = useState([]) //Returned array 
 
     async function getBillBoard() {
-        try {
-          setLoading(true)
+      try {
+        setLoading(true)
 
-          console.log(name)
-          const myName = '%'+ name +'%'
-          const { data, error } = await supabase.from('billboard_listings').select().ilike('name', myName)
-          setListArray(data)
-          if (error) throw error
-        } catch (error) {
-          alert('Error updating the data!')
-          console.log(error)
-        } finally {
-          setLoading(false)
-        }
+        console.log(name)
+        const myName = '%'+ name +'%'
+        const { data, error } = await supabase.from('billboard_listings').select().ilike('name', myName)
+        setListArray(data)
+        if (error) throw error
+      } catch (error) {
+        alert('Error updating the data!')
+        console.log(error)
+      } finally {
+        setLoading(false)
       }
-      return(
-        <>
-       <div>
-        <label>BillBoard Name</label>
-        <input
-          id="title"
-          onChange={(e) => setName(e.target.value)}
-        />
-      </div>
-      <button
-          className="button primary block"
-          onClick={() => {
-            getBillBoard({})
-          }}
-          disabled={loading}
-        >
+    }
+    
+    return(
+      <>
+        <div>
+          <label>Billboard Name</label>
+          <input id="title" onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <button className="button primary block" onClick={() => { getBillBoard({}) }} disabled={loading}>
           {loading ? 'Loading ...' : 'Create Billboard'}
         </button>
-      <BillboardList listingsArray={listArray}> </BillboardList>
+        <BillboardList listingsArray={listArray}> </BillboardList>
       </>
     )
 }
