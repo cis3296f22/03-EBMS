1f69f3152254fe4c376c9c39b54a0a3947bcca2b
Jacob Snarr <jacobsnarr@gmail.com>
2022-11-29 07:10:57 -0500

    Add redirect using query params

--- utils/supabaseClient.js
+++ utils/supabaseClient.js
@@ -3,7 +3,19 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://uomcmbqkndtmleifmays.supabase.co'
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVvbWNtYnFrbmR0bWxlaWZtYXlzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njg3MDUxNTYsImV4cCI6MTk4NDI4MTE1Nn0.WrbkHFkLzdv-YVIAG0qGbiwlHCqAdqr50OgQ83Y8aXs'
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+let supabase = createClient(supabaseUrl, supabaseAnonKey)
+
+export default supabase;
+
+export const handleLogin = async function(path) {
+  const { data, error } = await supabase.auth.signInWithOAuth({
+    provider: 'google',
+    options: {
+      // redirectTo: `http://localhost:3000` + path //PUBLISHING
+      redirectTo: 'http://localhost:3000' + path //FOR TESTING
+    }
+  })
+}
 
 //export default supabase
 
