eb0d4e5e59219648b7edc4f8da20dc9a70ac59db
Jacob Snarr <jacobsnarr@gmail.com>
2022-11-28 21:52:31 -0500

    Add empty billboards response

--- components/Billboard/BillboardList/BillboardList.jsx
+++ components/Billboard/BillboardList/BillboardList.jsx
@@ -3,6 +3,8 @@ import BillboardCard from '../BillboardListView/BillboardCard'
 import styles from './BillboardList.module.css'
 
 const BillboardList = ({ listingsArray }) => {
+  if (listingsArray.length == 0) return (<>No billboards</>)
+
   return (<div className={styles.billboardListContainer}>
     {React.Children.toArray(listingsArray.map((props) => {
       return <BillboardCard {...props}/>
