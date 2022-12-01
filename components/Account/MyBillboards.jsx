51e8d16368682c57f8a7c59d4bb89b47f498027f
timlopes17 <timlopes0817@gmail.com>
2022-11-30 20:08:32 -0500

    Cleaned up create and search page

--- components/Account/MyBillboards.jsx
+++ components/Account/MyBillboards.jsx
@@ -1,7 +1,7 @@
 
 import { useState, useEffect } from 'react'
 import { useSupabaseClient } from '@supabase/auth-helpers-react'
-import BillboardList from '../Billboard/BillboardList/BillboardList';
+import AccountBillboardList from '../Billboard/AccountBillboardList/AccountBillboardList';
 import styles from './MyBillboards.module.css'
 
 export default function Search({userId}) {
@@ -32,7 +32,7 @@ export default function Search({userId}) {
     return(
       <div className={styles.container}>
         <div>
-          <BillboardList listingsArray={listArray}> </BillboardList>
+          <AccountBillboardList listingsArray={listArray}> </AccountBillboardList>
         </div>
       </div>
     )
---
+++ components/Billboard/AccountBillboardList/AccountBillboardList.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import BillboardCard from '../BillboardListView/BillboardCard'
+import styles from './AccountBillboardList.module.css'
+import { useUser, useSupabaseClient } from '@supabase/auth-helpers-react'
+
+const BillboardList = ({ listingsArray, setCurrentSelection }) => {
+  const supabase = useSupabaseClient()
+
+  if (listingsArray.length == 0) return (<>No billboards</>)
+
+  async function handleDelete(id) {
+    try {
+      console.log(id)
+      const { error } = await supabase.from('billboard_listings').delete().eq('id', id)
+      if (error) throw error
+      alert('Billboard Deleted')
+    } 
+    catch (error) {
+      alert('Cannot Delete Billboard With Ad')
+      console.log(error)
+    } 
+}
+
+async function handleDisplay(id) {
+  try {
+    console.log(id)
+    const { data, error } = await supabase.from('ad_bookings').select('imgUrl').eq('billboardId', id)
+    if(data === null)
+      alert('No Ad On Billboard')
+    else{
+      console.log(data[0].imUrl)
+      window.open(data[0].imgUrl, '_blank')
+    }
+    if (error) throw error
+  } 
+  catch (error) {
+    alert('Error updating the data!')
+    console.log(error)
+  } 
+}
+
+  return (<div className={styles.billboardListContainer}>
+    {React.Children.toArray(listingsArray.map((props) => {
+      console.log(props.id)
+      return (
+        <div>
+          <BillboardCard {...props} setCurrentSelection={setCurrentSelection}/>
+          <button className={styles.button} onClick={() => {handleDisplay(props.id)}}>
+            Display
+          </button>
+          <button className={styles.button} onClick={() => {handleDelete(props.id)}}>
+            Delete
+          </button>
+        </div>
+      )
+    }))}
+  </div>)
+}
+
+export default BillboardList;
