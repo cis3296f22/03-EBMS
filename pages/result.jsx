5f313f8dd502c1fb742f177a505b5d7234b4771d
Jacob Snarr <jacobsnarr@gmail.com>
2022-12-01 02:31:35 -0500

    Fix purchase validation and payment type

--- pages/result.jsx
+++ pages/result.jsx
@@ -2,17 +2,22 @@ import Head from 'next/head'
 import { useRouter } from 'next/router'
 import React, {useState, useEffect} from 'react'
 import Header from '../components/Header/Header'
-import { useSession } from '@supabase/auth-helpers-react'
-import supabase, {handleLogin} from '../utils/supabaseClient';
+import { useUser, useSupabaseClient } from '@supabase/auth-helpers-react'
 
 export default function Buy() {
+  const supabase = useSupabaseClient()
+
   const router = useRouter()
   const {session_id} = router.query
 
   useEffect(() => {
-    // console.log(session_id)
-    let { error } = supabase.from('ad_bookings').select().update({'paymentVerified': true}).eq('sessionId', session_id)
-    if (error) throw error
+    async function loadData() {
+      console.log("data loading")
+      const { data, error } = await supabase.from('ad_bookings').update({'paymentVerified': true}).eq('id', 1).select()
+      if (error) console.log(error)
+      console.log(data)
+    }
+    loadData()
   }, [])
 
   return (
@@ -23,7 +28,7 @@ export default function Buy() {
         <link rel="icon" href="/adorado.ico" />
       </Head>
       <Header/>
-      <div style={{display:"flex", justifyContent:"center"}}>
+      <div style={{height: "100%", display:"flex", justifyContent:"center"}}>
 
         <h1> Purchase Successful! </h1>
       </div>
